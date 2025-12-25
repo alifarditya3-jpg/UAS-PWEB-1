d7de54f90b2feb6f52fda33ccd6886d9b6bad54d
alifarditya3-jpg <alifarditya3@gmail.com>
2025-12-25 10:55:40 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+    const question = item.querySelector(".faq-question");
+
+    question.addEventListener("click", () => {
+        item.classList.toggle("active");
+    });
+});
